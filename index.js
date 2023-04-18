315f80425a314375b74ec476a0799a2a91da12c2
AngkitHashamsa <angkithashamsa21@gmail.com>
2023-04-18 17:59:26 +0530

    raod added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const canvas = document.querySelector("#myCanvas");
-
+canvas.width = 200;
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(100, 100, 30, 50);
 
 animate();
 
 function animate() {
   canvas.height = window.innerHeight;
-  canvas.width = 200;
+
+  road.draw(ctx);
   car.update();
   car.draw(ctx);
   requestAnimationFrame(animate);
